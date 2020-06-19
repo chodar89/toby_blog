a4467cbec444c1ce110572258fc95a21af93351a
Tobiasz <tobiasz0889@gmail.com>
2020-06-19 16:42:50 +0100

    About page - set url pattern for about page - set hero img for about page - add content text to about page - change navbar django template logic for more clear

--- toby_blog/urls.py
+++ toby_blog/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog_posts.urls')),
-    # path('static/', include('static_pages.urls')),
+    path('', include('static_pages.urls')),
     path('', include('home_page.urls')),
 ]
