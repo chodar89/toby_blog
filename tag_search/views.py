61e12946942ac9dd474eac6304b8e4bfcf951e0e
Tobiasz <tobiasz0889@gmail.com>
2020-06-26 20:09:55 +0100

    Set up form search for tags inside the tag_search view

--- tag_search/views.py
+++ tag_search/views.py
@@ -11,7 +11,6 @@ class SearchView(View):
     def get(self, request, *args, **kwargs):
         tag_name = self.kwargs.get("tag")
         posts = Post.objects.filter(tags__tag=tag_name)
-        
         tags = Tag.objects.all().order_by('views')[:5]
         context = {
             'posts':posts,
@@ -20,4 +19,13 @@ class SearchView(View):
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
+        search_tag = self.request.POST.get("tag", None)
+        tags = Tag.objects.all().order_by('views')[:5]
+        if search_tag is not None:
+            print(search_tag)
+            posts = Post.objects.filter(tags__tag=search_tag.lower())
+        context = {
+            'posts':posts,
+            'tags': tags
+        }
         return render(request, self.template_name, context)
