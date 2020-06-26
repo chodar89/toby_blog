4d45666f3a7f409b08e70c63a697fa1775920835
Tobiasz <tobiasz0889@gmail.com>
2020-06-26 14:54:45 +0100

    Add class View for index page, add querysets and set index page to load content dynamicly

--- home_page/views.py
+++ home_page/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from blog_posts.models import Post
+
 # Create your views here.
 
 
-class HomeView(TemplateView):
+class HomeView(View):
     template_name = 'index.html'
+    def get(self, request, *args, **kwargs):
+        featured_post = Post.objects.filter(is_featured=True).order_by('-date')[:1]
+        latest_posts = Post.objects.all().order_by('-date')[:2]
+        context = {
+            'featured_post': featured_post,
+            'latest_posts': latest_posts
+        }
+        return render(request, self.template_name, context)
